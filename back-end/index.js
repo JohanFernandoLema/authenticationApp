7742884da1c735c59e0811abade875964c7b4610
JohanFernandoLema <johan.13fernando@gmail.com>
2023-10-16 22:11:40 -0400

    local mongo db connected towards the server

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,17 @@
 import express from 'express'
 import { MongoClient } from 'mongodb'
 
+// Middleware for enabling json
+app.use(express.json())
 // Connections for making the server
 const app = express()
-const url = 'mongodb://localhost:27017'
-const client = new MongoClient(url)
+
+const client = new MongoClient('mongodb://localhost:27017')
+await client.connect()
+
+//Connection to database
+const db = client.db()
+
 app.listen(4000, () => {
   console.log(`App successfully running on port: 4000`)
 })
