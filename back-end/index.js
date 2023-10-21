5e02a3df869af38e7dea19346f977d98248e13ad
JohanFernandoLema <johan.13fernando@gmail.com>
2023-10-21 09:17:20 -0400

    Dependencies for authentication process created

--- back-end/index.js
+++ back-end/index.js
@@ -1,16 +1,20 @@
 import express from 'express'
 import { MongoClient } from 'mongodb'
 
-// Middleware for enabling json
-app.use(express.json())
 // Connections for making the server
 const app = express()
 
-const client = new MongoClient('mongodb://localhost:27017')
-await client.connect()
+// Middleware for enabling json
+app.use(express.json())
 
-//Connection to database
-const db = client.db()
+app.get('/', (req, res) => {
+  res.send('It is connected that is why it is working')
+})
+// const client = new MongoClient('mongodb://localhost:27017/')
+// await client.connect()
+
+// //Connection to database
+// const db = client.db()
 
 app.listen(4000, () => {
   console.log(`App successfully running on port: 4000`)
