e3305e3491ef4e83652da9f32292c4aa604a611c
JohanFernandoLema <johan.13fernando@gmail.com>
2023-10-22 12:15:29 -0400

    files modified in ordeer to encypt information

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import { connectToDb } from './db.js'
 import dotenv from 'dotenv'
-// import { signUpPage } from './routes/singUpRoute.js'
 // Connections for making the server
 const app = express()
 
@@ -10,8 +9,6 @@ app.use(express.json())
 dotenv.config()
 
 // Database Connection
-
-// app.get('/signup', signUpPage)
 connectToDb(() => {
   console.log('Successfully running the local database')
   app.listen(4000, () => {
