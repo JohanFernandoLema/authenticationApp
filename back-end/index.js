cdd9f2c1863233f949f336ac89bebd76fce5b0ca
JohanFernandoLema <johan.13fernando@gmail.com>
2023-10-22 10:19:47 -0400

    conncetion to local db generated

--- back-end/index.js
+++ back-end/index.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import mongoose from 'mongoose'
+import { connectToDb } from './db.js'
 import dotenv from 'dotenv'
 // Connections for making the server
 const app = express()
@@ -9,20 +9,14 @@ app.use(express.json())
 dotenv.config()
 
 // Database Connection
-const cloudDb = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI)
-    console.log('Connected to mongo db cloud successfully')
-  } catch (err) {
-    throw err
-  }
-}
 
 app.get('/', (req, res) => {
   res.send('It is connected that is why it is working')
 })
 
-app.listen(4000, () => {
-  cloudDb()
-  console.log(`App successfully running on port: 4000`)
+connectToDb(() => {
+  console.log('Successfully running the local database')
+  app.listen(4000, () => {
+    console.log(`App successfully running on port: 4000`)
+  })
 })
