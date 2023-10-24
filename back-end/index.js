b0ec08c71e076e9790243795639e8290f338dca4
JohanFernandoLema <johan.13fernando@gmail.com>
2023-10-24 18:52:01 -0400

    database connection created - first URL called for registering new users

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,29 @@
 import express from 'express'
-const app = express()
+import bcrypt from 'bcrypt'
+import mongoose from 'mongoose'
 
-app.listen(process.env.PORT, () => {
-  console.log('Connected successfully to back-end')
+// Middlewares
+const app = express()
+app.use(express.json())
+
+// Connecting with local db
+
+const MONGO_URI = 'mongodb://127.0.0.1:27017/react-auth-db'
+
+app.post('/api/signup', async (req, res) => {
+  const { email, password } = req.body
+  const passwordHash = await bcrypt.hash(password, 10)
+  const startingInfo = { hairColor: '', favoriteFood: '', bio: '' }
 })
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('You are connected to the database')
+    app.listen(process.env.PORT, () => {
+      console.log('Connected successfully to back-end')
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
