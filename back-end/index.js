8e1ee683c3d568add292af014fd43d0cf621930b
JohanFernandoLema <johan.13fernando@gmail.com>
2023-10-23 22:38:53 -0400

    peroject back end modified - re-started from scratch

--- back-end/index.js
+++ back-end/index.js
@@ -1,23 +1,6 @@
 import express from 'express'
-import { connectToDb } from './db.js'
-import homePage from './routes/homePage.js'
-// import signUpPage from './routes/singUpRoute.js'
-import dotenv from 'dotenv'
-// Connections for making the server
 const app = express()
 
-// Middleware for enabling json
-app.use(express.json())
-dotenv.config()
-
-// URLs
-app.get('/', homePage)
-// app.post('/api/signup', signUpPage)
-
-// Database Connection
-connectToDb(() => {
-  console.log('Successfully running the local database')
-  app.listen(4000, () => {
-    console.log(`App successfully running on port: 4000`)
-  })
+app.listen(process.env.PORT, () => {
+  console.log('Connected successfully to back-end')
 })
--- back-end/routes/homePage.js
+++
@@ -1,8 +0,0 @@
-import express from 'express'
-const router = express.Router()
-
-router.get('/', (req, res) => {
-  res.send('You have reached the home page')
-})
-
-export default router
