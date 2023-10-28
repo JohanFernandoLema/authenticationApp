d4c0f792bb0a0106f1138c4cf71ff148ff22c45e
JohanFernandoLema <johan.13fernando@gmail.com>
2023-10-28 15:32:39 -0400

    log in api completed - check if user is allow to log in

--- back-end/routes/LogInRoute.js
+++ back-end/routes/LogInRoute.js
@@ -1,13 +1,27 @@
-// import express from 'express'
-// import bcrypt from 'bcrypt'
-// import jwt from 'jsonwebtoken'
-// import
+import express from 'express'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+import { User } from '../models/UserSchema'
 
-// const router = express.Router()
+const router = express.Router()
 
-// router.post('api/login', async (req, res) => {
-//   const { email, password } = req.body
+router.post('api/login', async (req, res) => {
+  const { email, password } = req.body
 
-// })
+  const user = User.findOne({ email })
 
-// export default router
+  if (!checkEmail) return res.send(401)
+
+  const { _id: id, passwordHash } = user
+
+  const isCorrect = await bcrypt.compare(password, passwordHash)
+
+  const token = jwt.sign({ id, email }, 'shhhhhhhhh', { expiresIn: '2h' })
+  if (isCorrect) {
+    return res.status(200).json({ token })
+  } else {
+    return res.status(401)
+  }
+})
+
+export default router
