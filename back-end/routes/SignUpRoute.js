a22436f8e9e949e3976d168fe76e0273424d4a3b
JohanFernandoLema <johan.13fernando@gmail.com>
2023-10-28 14:47:36 -0400

    user is being created succesfully - can not create duplicated users

--- back-end/routes/SignUpRoute.js
+++ back-end/routes/SignUpRoute.js
@@ -8,25 +8,24 @@ router.post('/api/signup', async (req, res) => {
   try {
     const { email, password } = req.body
     const existingUser = await SignUpSchema.findOne({ email })
-    if (existingUser) {
-      res.sendStatus(401).send('User already exists')
-    }
 
+    if (existingUser) {
+      return res.send('User already exists')
+    }
     const hashPassword = await bcrypt.hash(password, 10)
 
     // Save user in DB
     const user = await SignUpSchema.create({ email, password: hashPassword })
-
-    // Generate a token for user and send it
-    const token = jwt.sign({ id: user._id, email }, process.env.JWT_SECRET, {
-      expiresIn: '2h',
-    })
-    user.token = token
-    user.email = email
-    res.status(201).json({ token })
+    res.send(user)
+    //     // Generate a token for user and send it
+    //     const token = jwt.sign({ id: user._id, email }, process.env.JWT_SECRET, {
+    //       expiresIn: '2h',
+    //     })
+    //     user.token = token
+    //     user.email = email
+    //     res.status(201).json({ token })
   } catch (err) {
     res.send(err)
   }
 })
-
 export default router
