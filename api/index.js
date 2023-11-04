c8daa06730d8a3c5f842a7c5450a81ad6593bda9
JohanFernandoLema <johan.13fernando@gmail.com>
2023-11-03 22:42:02 -0400

    error in mongoose package to get fixed

--- api/index.js
+++ api/index.js
@@ -1,8 +1,22 @@
 import express from 'express'
+import mongoose from 'mongoose'
 
 const app = express()
 const PORT = 3000
 
-app.listen(PORT, () => {
-  console.log(`Application running on PORT: ${PORT}`)
-})
+// Middlewares
+app.use(express.json())
+
+const MONGO_URI = 'mongodb://127.0.0.1:27017/react-auth-db'
+
+mongoose
+  .connect(MONGO_URI)
+  .then(() => {
+    console.log('Successfully connected in your local DB')
+    app.listen(PORT, () => {
+      console.log(`Application running on PORT: ${PORT}`)
+    })
+  })
+  .catch((err) => {
+    console.log(err)
+  })
