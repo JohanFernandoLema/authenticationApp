ec3ddc9f10fe7a08c5ac001593bf0258d3b85578
JohanFernandoLema <johan.13fernando@gmail.com>
2023-11-03 21:54:55 -0400

    folder modification in order to fix errors

--- front-end-authentication/src/auth/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate, Route } from 'react-router-dom'
-import { useUser } from './useUser'
-export const PrivateRoute = (props) => {
-  const user = useUser() // determine if authorized, from context or however you're doing it
-  // If authorized, return an outlet that will render child elements
-  // If not, return element that will navigate to login page
-  if (!user) return <Navigate to="/login" />
-  return <Route {...props} />
-}
