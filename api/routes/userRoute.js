ff3907c074320145b9e07410ff9f3224f93afe49
JohanFernandoLema <johan.13fernando@gmail.com>
2023-11-06 21:35:38 -0500

    user register URI created - not tested yet

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,8 +1,20 @@
 import express from 'express'
+import User from '../models/User.js'
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  console.log('Home page is working - send more data :)')
+  res.send('Home page is working - send more data :)')
+})
+
+router.post('/signup', (req, res) => {
+  const { email, password } = req.body
+
+  const verifyEmail = User.findOne({ email })
+  if (!verifyEmail) return res.sendStatus(404)
+
+  const saveUser = User.create({ email: email, password: password })
+
+  res.send(saveUser)
 })
 
 export default router
