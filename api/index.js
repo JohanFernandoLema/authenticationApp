f0300eb07f539a64868e08fd4684e3d5d2594bd1
JohanFernandoLema <johan.13fernando@gmail.com>
2023-11-06 21:24:11 -0500

    index file modified in order to import user route

--- api/index.js
+++ api/index.js
@@ -1,11 +1,15 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import userRoute from './routes/userRoute.js'
 
 const app = express()
 const PORT = 3000
 
 // Middlewares
 app.use(express.json())
+
+// CALLING URIs
+app.get('/', userRoute)
 
 const MONGO_URI = 'mongodb://127.0.0.1:27017/react-auth-db'
 
