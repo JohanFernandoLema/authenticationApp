5b1933ab93a1244931189f9d4fd92a2968bd5616
JohanFernandoLema <johan.13fernando@gmail.com>
2023-11-08 11:09:21 -0500

    token input added into our user schema in order to add auth porpuses

--- api/models/User.js
+++ api/models/User.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose'
 const User = new mongoose.Schema({
   email: { type: String },
   password: { type: String },
+  token: { type: String },
 })
 
 const UserSchema = mongoose.model('users', User)
