9c1cb0b2b6e277b0af5c0b0b46a320b863c99fe6
JohanFernandoLema <johan.13fernando@gmail.com>
2023-11-08 13:48:28 -0500

    login page created for user authentication

---
+++ client/src/pages/Login.js
@@ -0,0 +1,14 @@
+export const Login = () => {
+  return (
+    <div>
+      <label htmlFor="userEmail">
+        Email:
+        <input type="text" placeholder="Input your email" />
+      </label>
+      <label htmlFor="userPassword">
+        Email:
+        <input type="text" placeholder="Input your password" />
+      </label>
+    </div>
+  )
+}
