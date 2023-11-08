34796bb8b4c47134136080fefed4df08ccd51ef6
JohanFernandoLema <johan.13fernando@gmail.com>
2023-11-08 11:05:51 -0500

    user registration created - jwt token will be retrieved for getting to know server is working

--- api/routes/userRoute.js
+++ api/routes/userRoute.js
@@ -1,20 +1,41 @@
 import express from 'express'
 import User from '../models/User.js'
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
 const router = express.Router()
 
 router.get('/', (req, res) => {
   res.send('Home page is working - send more data :)')
 })
 
-router.post('/signup', (req, res) => {
+router.post('/signup', async (req, res) => {
   const { email, password } = req.body
 
-  const verifyEmail = User.findOne({ email })
-  if (!verifyEmail) return res.sendStatus(404)
+  // Verify if email exists to avoid multiple user creation
+  const verifyEmail = await User.findOne({ email })
+  if (verifyEmail) {
+    return res.send('Verify the information is correct')
+  }
 
-  const saveUser = User.create({ email: email, password: password })
+  // Hash Password
+  const hashPassword = await bcrypt.hash(password, 10)
 
-  res.send(saveUser)
+  // Save user and display as json
+  const user = await User.create({ email: email, password: hashPassword })
+
+  // Use jwt to generate auth token
+  const token = jwt.sign(
+    {
+      id: user._id,
+      email: user.email,
+    },
+    'secret',
+    { expiresIn: '2h' }
+  )
+  user.token = token
+  user.password = undefined
+
+  return res.json({user.token})
 })
 
 export default router
