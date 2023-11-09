3c4d28981d60c833bd84612c7db68a39b1736b89
JohanFernandoLema <johan.13fernando@gmail.com>
2023-11-08 21:00:03 -0500

    register page name conventions replaced by understandable ones

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -4,33 +4,51 @@ const Register = () => {
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
   const [error, setError] = useState('')
+
+  const onSignUpClicked = async () => {
+    alert('Sign up button not implemented yet')
+  }
+
   return (
     <div>
       {error && <p>{error}</p>}
       <div>
         <label htmlFor="userEmail">
+          Email:
           <input
             type="text"
             value={email}
             onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter your email"
           />
         </label>
         <label htmlFor="userPassword">
+          Password:
           <input
             type="password"
             value={password}
             onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter your password"
           />
         </label>
         <label htmlFor="userConfirmPassword">
+          Confirm Password:
           <input
             type="confirmPassword"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
+            placeholder="Re-enter your password"
           />
         </label>
       </div>
-      <button>I have an account already</button>
+      <button
+        onClick={onSignUpClicked}
+        disabled={!email || !password || password !== confirmPassword}
+      >
+        Sign Up
+      </button>
+
+      <p>Already have an account?</p>
     </div>
   )
 }
