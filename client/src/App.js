69bf94d2fd446f5f9a5240f04c716a93d487e125
JohanFernandoLema <johan.13fernando@gmail.com>
2023-11-10 19:23:04 -0500

    private route imported inside our app file

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import './App.css'
 import LoginPage from './pages/Login.js'
 import RegisterPage from './pages/Register.js'
 import UserInfoPage from './pages/UserInfoPage.js'
+import { PrivateRoute } from './auth/PrivateRoute.js'
 import { Route, Routes, BrowserRouter } from 'react-router-dom'
 
 function App() {
@@ -9,7 +10,14 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<UserInfoPage />} />
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <UserInfoPage />
+              </PrivateRoute>
+            }
+          />
           <Route path={'/login'} element={<LoginPage />} />
           <Route path={'/register'} element={<RegisterPage />} />
         </Routes>
