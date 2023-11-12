61a135142ba51b70efcad59043e628100a7d7ab4
JohanFernandoLema <johan.13fernando@gmail.com>
2023-11-12 09:57:07 -0500

    email authentication generated - client server will move forward as long as there is a user

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,12 +1,26 @@
 import { useState } from 'react'
+import { useToken } from '../auth/useToken'
+import { useNavigate } from 'react-router-dom' // version 5.2.0
+
+import axios from 'axios'
 const Register = () => {
+  const [token, setToken] = useToken()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [confirmPassword, setConfirmPassword] = useState('')
   const [error, setError] = useState('')
 
+  const navigate = useNavigate()
+
   const onSignUpClicked = async () => {
-    alert('Sign up button not implemented yet')
+    const response = await axios.post('http://localhost:4000/signup', {
+      email: email,
+      password: password,
+    })
+
+    const { token } = response.data
+    setToken(token)
+    navigate('/')
   }
 
   return (
@@ -34,7 +48,7 @@ const Register = () => {
         <label htmlFor="userConfirmPassword">
           Confirm Password:
           <input
-            type="confirmPassword"
+            type="password"
             value={confirmPassword}
             onChange={(e) => setConfirmPassword(e.target.value)}
             placeholder="Re-enter your password"
