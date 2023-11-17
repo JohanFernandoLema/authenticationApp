eb36fb064253ba5400a0a368e2e095f6ef1cd239
JohanFernandoLema <johan.13fernando@gmail.com>
2023-11-17 17:43:22 -0500

    name convention modified since there was some typos not allowing to run the server

--- api/server.js
+++ api/server.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import userRoutes from './routes/userRoutes.js'
 
 dotenv.config()
 
 const app = express()
 
 const PORT = process.env.PORT || 5000
+
+app.post('/api/users', userRoutes)
 
 app.listen(PORT, () => console.log(`The server is running on port: ${PORT}`))
