5ee4e862aa5fcc3efc28d7d46914c6ff7201e2b8
JohanFernandoLema <johan.13fernando@gmail.com>
2023-11-17 20:39:39 -0500

    express async handler installed as dependency for avoiding boilerplate code

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,5 +1,7 @@
-const authUser = (req, res) => {
+import asyncHandler from 'express-async-handler'
+
+const authUser = asyncHandler(async (req, res) => {
   res.status(200).json({ message: 'Authentication endpoint' })
-}
+})
 
 export { authUser }
